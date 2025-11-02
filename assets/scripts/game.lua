a625db97b6d1f3d8b4aecb35297c8b7895d2cb4c
Tomasz Klimek <tomkl584@gmail.com>
2025-11-02 20:33:45 +0100

    Tile Puzzle

--- assets/scripts/game.lua
+++ assets/scripts/game.lua
@@ -1,121 +1,183 @@
+Direction = {UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3}
+
+Directions = {
+    [Direction.UP] = {x = 0, y = -1},
+    [Direction.DOWN] = {x = 0, y = 1},
+    [Direction.LEFT] = {x = -1, y = 0},
+    [Direction.RIGHT] = {x = 1, y = 0}
+}
+
 function Engine.init()
-    Engine.World.loadMap("Exterior")
-    player = Engine.TilesetSprite.new("exterior.tsj", 69, 645, 20, 26)
+    Texture = Engine.Texture.load("puzzle.png")
+
+    local margin = 5
+    local tileSize = 640
+    GridSize = 3
+    Tiles = {}
+    Board = {}
+
+    -- First row
+    Board[1] = {}
+    for x=1,GridSize - 1 do
+        AddTile(x, 1, margin, tileSize)
+    end
+    Board[1][GridSize] = {tileId = 0} -- Empty tile
+
+    -- Other rows
+    for y=2,GridSize do
+        Board[y] = {}
+        for x=1,GridSize do
+            AddTile(x, y, margin, tileSize)
+        end
+    end
+
+    UpdateTiles()
 end
 
-local pointers = {}
+function AddTile(x, y, margin, tileSize)
+    Tiles[#Tiles + 1] = Engine.Sprite.new(Texture,
+            margin + (x - 1) * (tileSize + margin),
+            margin + (y - 1) * (tileSize + margin), tileSize, tileSize)
+    Board[y][x] = {tileId = #Tiles}
+end
+
+function UpdateTiles()
+    local windowSizeX, windowSizeY = Engine.Window.getSize()
+    TileSize = math.floor(math.min(windowSizeX, windowSizeY) / GridSize * 0.95)
+    local margin = math.floor(windowSizeY * 0.005)
+    local boardSize = TileSize * GridSize + margin * (GridSize - 1)
+    local startX = math.floor(windowSizeX / 2 - boardSize / 2)
+    local startY = math.floor(windowSizeY / 2 - boardSize / 2)
+
+    for y=1,GridSize do
+        for x=1,GridSize do
+            local tile = Board[y][x]
+            local tileSprite = Tiles[tile.tileId]
+            if tileSprite then
+                tileSprite:setSize(TileSize, TileSize)
+
+                local space = (TileSize + margin)
+
+                local tilePosX = startX + (x - 1) * space
+                local tilePosY = startY + (y - 1) * space
+
+                if tile.move then
+                    local moveDirection = Directions[tile.move]
+                    local animation = AnimationCurve(tile.movePhase)
+                    tilePosX = tilePosX + animation * moveDirection.x * space
+                    tilePosY = tilePosY + animation * moveDirection.y * space
+                end
+
+                tileSprite:setPos(tilePosX, tilePosY)
+            end
+        end
+    end
+end
+
+function AnimationCurve(phase)
+    if phase <= 0.5 then
+        return 2 * phase * phase
+    else
+        phase = phase - 0.5
+        return 2 * phase * (1 - phase) + 0.5
+    end
+end
+
+function GetTileFromCoords(x, y)
+    for gridX=1,GridSize do
+        for gridY=1,GridSize do
+            local tile = Board[gridY][gridX]
+            local tileSprite = Tiles[tile.tileId]
+            if tileSprite then
+                local tileX, tileY = tileSprite:getPos()
+                if x >= tileX and x <= tileX + TileSize
+                        and y >= tileY and y <= tileY + TileSize
+                then
+                    return {x= gridX, y = gridY}
+                end
+            end
+        end
+    end
+    return nil
+end
+
+function PressTile(gridX, gridY)
+    local tile = Board[gridY][gridX]
+    local nearbyTiles = {
+        {x = gridX, y = gridY - 1, direction = Direction.UP},
+        {x = gridX, y = gridY + 1, direction = Direction.DOWN},
+        {x = gridX - 1, y = gridY, direction = Direction.LEFT},
+        {x = gridX + 1, y = gridY, direction = Direction.RIGHT}
+    }
+
+    for _,nearbyTile in ipairs(nearbyTiles) do
+        local row = Board[nearbyTile.y]
+        if row then
+            local checkedTile = row[nearbyTile.x]
+            if checkedTile and checkedTile.tileId == 0 then
+                tile.move = nearbyTile.direction
+                tile.movePhase = 0.0
+                Moving = true
+                return
+            end
+        end
+    end
+end
 
 function Engine.timeStep(time)
-    local up_pressed = false
-    local down_pressed = false
-    local left_pressed = false
-    local right_pressed = false
+    for y=1,GridSize do
+        for x=1,GridSize do
+            local tile = Board[y][x]
+            if tile.tileId ~= 0 and tile.move then
+                tile.movePhase = tile.movePhase + time * 4.0
+                if tile.movePhase >= 1.0 then
+                    local moveDirection = Directions[tile.move]
+                    local destination = Board[y + moveDirection.y][x + moveDirection.x]
 
-    if Engine.Keyboard.isKeyPressed("w") or Engine.Keyboard.isKeyPressed("up") then
-        up_pressed = true
-    end
-    if Engine.Keyboard.isKeyPressed("s") or Engine.Keyboard.isKeyPressed("down") then
-        down_pressed = true
-    end
-    if Engine.Keyboard.isKeyPressed("a") or Engine.Keyboard.isKeyPressed("left") then
-        left_pressed = true
-    end
-    if Engine.Keyboard.isKeyPressed("d") or Engine.Keyboard.isKeyPressed("right") then
-        right_pressed = true
-    end
+                    Moving = false
+                    tile.move = nil
+                    tile.movePhase = nil
 
-    for _,pos in pairs(pointers) do
-        local controlButtonSize = Engine.World.getScreenTileSize() * 3
-        local windowSizeX, windowSizeY = Engine.Window.getSize()
-        if pos.y <= controlButtonSize then
-            up_pressed = true
-        end
-        if pos.y >= windowSizeY - controlButtonSize then
-            down_pressed = true
-        end
-        if pos.x <= controlButtonSize then
-            left_pressed = true
-        end
-        if pos.x >= windowSizeX - controlButtonSize then
-            right_pressed = true
+                    destination.tileId, tile.tileId = tile.tileId, 0
+                end
+            end
         end
     end
-
-    local move = time * 3.0
-    local moveX, moveY = false, false
-    if up_pressed and down_pressed then
-        up_pressed = false
-        down_pressed = false
-    end
-    if left_pressed and right_pressed then
-        left_pressed = false
-        right_pressed = false
-    end
-    if up_pressed and left_pressed then
-        local root2 = math.sqrt(2)
-        player:move(-move / root2, -move / root2)
-        moveX, moveY = true, true
-    elseif up_pressed and right_pressed then
-        local root2 = math.sqrt(2)
-        player:move(move / root2, -move / root2)
-        moveX, moveY = true, true
-    elseif down_pressed and right_pressed then
-        local root2 = math.sqrt(2)
-        player:move(move / root2, move / root2)
-        moveX, moveY = true, true
-    elseif down_pressed and left_pressed then
-        local root2 = math.sqrt(2)
-        player:move(-move / root2, move / root2)
-        moveX, moveY = true, true
-    elseif up_pressed then
-        player:move(0, -move)
-        moveX, moveY = false, true
-    elseif down_pressed then
-        player:move(0, move)
-        moveX, moveY = false, true
-    elseif left_pressed then
-        player:move(-move, 0)
-        moveX, moveY = true, false
-    elseif right_pressed then
-        player:move(move, 0)
-        moveX, moveY = true, false
-    end
-    
-    if not moveX or not moveY then
-        local posX, posY = player:getPos()
-        local screenTileSize = Engine.World.getScreenTileSize()
-        posX, posY = posX * screenTileSize, posY * screenTileSize
-        if not moveX then
-            posX = math.floor(posX + 0.5)
-        end
-        if not moveY then
-            posY = math.floor(posY + 0.5)
-        end
-        player:setPos(posX / screenTileSize, posY / screenTileSize)
-    end
-
-    local playerPosX, playerPosY = player:getPos()
-    local cameraPosX, cameraPosY = Engine.World.getCameraPos()
-    local larp_factor = 6 * time
-    local newCameraPosX = playerPosX * larp_factor + cameraPosX * (1 - larp_factor)
-    local newCameraPosY = playerPosY * larp_factor + cameraPosY * (1 - larp_factor)
-    Engine.World.setCameraPos(newCameraPosX, newCameraPosY)
+    UpdateTiles()
 end
 
+function Engine.Mouse.buttonPressed(button, x, y)
+    if Moving then return end
+    local foundTile = GetTileFromCoords(x, y)
+    if foundTile then
+        PressTile(foundTile.x, foundTile.y)
+    end
+end
+
+Pointers = {}
+
 function Engine.Touchscreen.pointerDown(x, y, id)
-    pointers[id] = {x = x, y = y}
+    local foundTile = GetTileFromCoords(x, y)
+    if foundTile then
+        Pointers[id] = Board[foundTile.y][foundTile.x];
+    end
 end
 
 function Engine.Touchscreen.pointerUp(x, y, id)
-    if pointers[id] == nil then error("pointer") end
-    pointers[id] = nil
+    local tile = Pointers[id]
+    if tile then
+        local foundTile = GetTileFromCoords(x, y)
+        if foundTile and not Moving and tile == Board[foundTile.y][foundTile.x] then
+            PressTile(foundTile.x, foundTile.y)
+        end
+        Pointers[id] = nil
+    end
 end
 
 function Engine.Touchscreen.pointerMove(x, y, id)
-    if pointers[id] == nil then error("pointer") end
-    pointers[id] = {x = x, y = y}
+
 end
 
 function Engine.Touchscreen.pointerCancel()
-    pointers = {}
+
 end
